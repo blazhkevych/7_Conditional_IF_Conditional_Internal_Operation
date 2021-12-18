12efab9594a3623b096b4e9e292a86ad12d3dfa2
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2021-12-18 19:32:50 +0200

    2 задачка

--- task_2/task_2.cpp
+++ task_2/task_2.cpp
@@ -13,5 +13,29 @@ int main()
 {
 	setlocale(LC_ALL, ""); //работает только с потоком вывода
 
+	cout << "Введите координаты вершин прямоугольника. "
+		<< "\nкоординаты левого верхнего угла: " << endl;
+	int upper_left_corner{ 0 }, lower_right_corner = { 0 };
+	cin >> upper_left_corner;
+
+	cout << "\nкоординаты правого нижнего угла: " << endl;
+	cin >> lower_right_corner;
+
+	cout << "Введите координаты точки \"Х\": " << endl;
+	int point_coordinates_x = { 0 };
+	cin >> point_coordinates_x;
+
+	cout << "Введите координаты точки \"Y\": " << endl;
+	int point_coordinates_y = { 0 };
+	cin >> point_coordinates_y;
+
+
+
+
+
 	return 0;
 }
+
+/*
+вывести в консоли картинку с пары про виндовскую систему координат
+*/
