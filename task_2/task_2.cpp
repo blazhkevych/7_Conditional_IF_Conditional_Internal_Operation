635b178578f0507015dcddcda7d02bd18d8172ce
Виктор Блажкевич <blazhkevych.va@gmail.com>
2021-12-20 19:00:38 +0200

    +2 задача

--- task_2/task_2.cpp
+++ task_2/task_2.cpp
@@ -13,29 +13,37 @@ int main()
 {
 	setlocale(LC_ALL, ""); //работает только с потоком вывода
 
-	cout << "Введите координаты вершин прямоугольника. "
-		<< "\nкоординаты левого верхнего угла: " << endl;
-	int upper_left_corner{ 0 }, lower_right_corner = { 0 };
-	cin >> upper_left_corner;
+	int upper_left_corner_x{ 0 }, upper_left_corner_y{ 0 }, lower_right_corner_x{ 0 }, lower_right_corner_y{ 0 };
+	cout << "Введите координаты вершин прямоугольника: "
+		<< "\nкоординаты левого верхнего угла по оси Х: " << endl;
+	cin >> upper_left_corner_x;
+	cout << "координаты левого верхнего угла по оси Y: " << endl;
+	cin >> upper_left_corner_y;
 
-	cout << "\nкоординаты правого нижнего угла: " << endl;
-	cin >> lower_right_corner;
+	cout << "\nкоординаты правого нижнего угла по оси Х: " << endl;
+	cin >> lower_right_corner_x;
+	cout << "координаты правого нижнего угла по оси Y: " << endl;
+	cin >> lower_right_corner_y;
 
-	cout << "Введите координаты точки \"Х\": " << endl;
-	int point_coordinates_x = { 0 };
+	cout << "\nВедите координаты Вашей \"точки\" :" << endl;
+
+	cout << "координаты точки по оси Х: " << endl;
+	int point_coordinates_x{ 0 };
 	cin >> point_coordinates_x;
 
-	cout << "Введите координаты точки \"Y\": " << endl;
-	int point_coordinates_y = { 0 };
+	cout << "координаты точки по оси Y: " << endl;
+	int point_coordinates_y{ 0 };
 	cin >> point_coordinates_y;
 
+	cout << "\nПринадлежит ли эта точка указанному выше прямоугольнику ?" << endl;
 
-
-
+	if (point_coordinates_x >= upper_left_corner_x
+		&& point_coordinates_x <= lower_right_corner_x
+		&& point_coordinates_y >= upper_left_corner_y
+		&& point_coordinates_y <= lower_right_corner_y)
+		cout << "\nПринадлежит." << endl;
+	else
+		cout << "Не принадлежит." << endl;
 
 	return 0;
 }
-
-/*
-вывести в консоли картинку с пары про виндовскую систему координат
-*/
